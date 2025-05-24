f99650ce5bcbe3579b53d7719cf9eee945127fcf
Martin Hehl <mahe@terraex.de>
2025-05-24 09:38:23 +0200

    fixed layout manager

--- trackMyStory/tMS/Layouts/ResponsiveLayoutManager.cs
+++ trackMyStory/tMS/Layouts/ResponsiveLayoutManager.cs
@@ -2,20 +2,13 @@
 
 namespace tMS.Layouts;
 
-public class ResponsiveLayoutManager : LayoutManager
+public class ResponsiveLayoutManager(Microsoft.Maui.ILayout layout) : LayoutManager(layout)
 {
-    private readonly Microsoft.Maui.ILayout _layout;
+    private readonly Microsoft.Maui.ILayout _layout = layout;
 
     public int HorizontalThreshold;
     public int HorizontalSpacing;
     public int VerticalSpacing;
-
-    private Dictionary<IView, Size> childSizeMap = new Dictionary<IView, Size>();
-
-    public ResponsiveLayoutManager(Microsoft.Maui.ILayout layout) : base(layout)
-    {
-        _layout = layout;
-    }
 
     public override Size Measure(double widthConstraint, double heightConstraint)
     {
@@ -27,7 +20,6 @@ public class ResponsiveLayoutManager : LayoutManager
             {
                 Size childSize = child.Measure(widthConstraint, heightConstraint);
                 requiredHeight += childSize.Height;
-                childSizeMap[child] = childSize;
             }
             return new Size(widthConstraint, requiredHeight + totalSpacing);
         }
@@ -38,7 +30,6 @@ public class ResponsiveLayoutManager : LayoutManager
             {
                 Size childSize = child.Measure((widthConstraint - totalSpacing) / _layout.Count, heightConstraint);
                 requiredHeight = Math.Max(requiredHeight, childSize.Height);
-                childSizeMap[child] = childSize;
             }
             return new Size(widthConstraint, requiredHeight);
         }
@@ -49,13 +40,12 @@ public class ResponsiveLayoutManager : LayoutManager
         double requiredHeight = 0;
         if (bounds.Width < HorizontalThreshold) // vertical mode
         {
-            var totalSpacing = VerticalSpacing * (_layout.Count - 1);
             var ew = bounds.Width;
             var i = 0;
 
             foreach (IView child in _layout)
             {
-                Size childSize = child.Arrange(new Rect(0, requiredHeight, ew, childSizeMap[child].Height));
+                Size childSize = child.Arrange(new Rect(0, requiredHeight, ew, child.DesiredSize.Height));
                 requiredHeight += childSize.Height + VerticalSpacing;
                 ++i;
             }
@@ -65,7 +55,7 @@ public class ResponsiveLayoutManager : LayoutManager
             var totalSpacing = HorizontalSpacing * (_layout.Count - 1);
             var ew = (bounds.Width - totalSpacing) / _layout.Count;
             var i = 0;
-        
+
             foreach (IView child in _layout)
             {
                 Size childSize = child.Arrange(new Rect((ew + HorizontalSpacing) * i, 0, ew, bounds.Height));
