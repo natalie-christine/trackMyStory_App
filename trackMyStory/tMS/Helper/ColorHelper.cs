5d09c903febbf3274ecb819ad904013daf25bb1c
mahe <mahe@terraex.de>
2025-06-04 21:45:37 +0200

    more colors

--- trackMyStory/tMS/Helper/ColorHelper.cs
+++ trackMyStory/tMS/Helper/ColorHelper.cs
@@ -18,6 +18,13 @@ public class ColorHelper
         return Color.FromRgb(red, green, blue);
     }
 
+    public static Color CreateTextColor(Color color)
+    {
+        return color.GetLuminosity() > 0.5 ?
+                        color.WithLuminosity(0f) :
+                        color.WithLuminosity(1f);
+    }
+
     public static void SetColor(Color? color, Color? color2)
     {
         static void SetColors(string p, Color c)
@@ -27,9 +34,7 @@ public class ColorHelper
                 Application.Current.Resources[p] = c.ToHex();
                 Application.Current.Resources[p + "Light"] = c.WithLuminosity(c.GetLuminosity() + 0.2f).ToHex();
                 Application.Current.Resources[p + "Dark"] = c.WithLuminosity(c.GetLuminosity() - 0.2f).ToHex();
-                Application.Current.Resources[p + "Text"] = c.GetLuminosity() > 0.5 ?
-                        c.WithLuminosity(0f).ToHex() :
-                        c.WithLuminosity(1f).ToHex();
+                Application.Current.Resources[p + "Text"] = CreateTextColor(c).ToHex();
 
                 Application.Current.Resources[p + "Brush"] = new SolidColorBrush(c);
             }
--- trackMyStory/tMS/Pages/AddCategoryPage.xaml.cs
+++ trackMyStory/tMS/Pages/AddCategoryPage.xaml.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Maui.Views;
 using System.Threading.Tasks;
+using tMS.Helper;
 
 namespace tMS.Pages;
 
@@ -8,11 +9,18 @@ public partial class AddCategoryPage : Popup
 	public AddCategoryPage()
 	{
 		InitializeComponent();
-	}
+        Color color = ColorHelper.CreateRandomColor();
+        ColorPickerCategorie.Red = color.Red;
+        ColorPickerCategorie.Green = color.Green;
+        ColorPickerCategorie.Blue = color.Blue;
+        ColorPickerCategorie_ColorChanged(null, null);
+    }
 
     private void ColorPickerCategorie_ColorChanged(object sender, EventArgs e)
     {
-
+        Color color = Color.FromRgb(ColorPickerCategorie.Red, ColorPickerCategorie.Green, ColorPickerCategorie.Blue);
+        entry.BackgroundColor = color;
+        entry.TextColor = ColorHelper.CreateTextColor(color);
     }
 
     private void Button_Clicked(object sender, EventArgs e)
